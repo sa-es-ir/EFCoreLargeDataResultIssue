d5ecbdbeffce0764af534ff4d1668ed72fe38e9f
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-10-19 14:58:24 +0700

    add benchmark

--- PostgresqlProvider/Program.cs
+++ PostgresqlProvider/Program.cs
@@ -1,16 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Domain;
+﻿using BenchmarkDotNet.Running;
 using PostgresqlProvider;
 
-Console.WriteLine("Hello, World!");
-
-var db = new PostgresqlDbContext();
-
-var textTable2MB = new TextTable2MB { Text = "TextTable2MB" };
-db.TextTable2MB.Add(textTable2MB);
-
-await db.SaveChangesAsync();
-
-var t = await db.TextTable2MB.FindAsync(textTable2MB.Id);
-
-Console.ReadLine();
+Console.WriteLine(BenchmarkRunner.Run<EFBenchmark>());
