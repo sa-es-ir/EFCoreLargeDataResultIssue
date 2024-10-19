d4f16922895f5c578398752460668abf4c679efc
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-10-19 12:58:35 +0700

    add entities

---
+++ Domain/Entities.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class TextTable2MB
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+    }
+
+    public class TextTable5MB
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+    }
+}
