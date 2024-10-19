b48faffefc6b8ba51460e7a4c07e9a33b23cfaf3
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-10-19 12:58:11 +0700

    add dbcontext

---
+++ PostgresqlProvider/PostgresqlDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PostgresqlProvider;
+
+public class PostgresqlDbContext : DbContext
+{
+    public PostgresqlDbContext(DbContextOptions<PostgresqlDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=EFLargeDataDb;Username=postgres;Password=postgres");
+    }
+
+    public DbSet<TextTable2MB> TextTable2MB { get; set; } = null!;
+
+    public DbSet<TextTable5MB> TextTable5MB { get; set; } = null!;
+}
+
+
+public class PostgresqlDbContextDesignFactory : IDesignTimeDbContextFactory<PostgresqlDbContext>
+{
+    public PostgresqlDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PostgresqlDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=EFLargeDataDb;Username=postgres;Password=postgres");
+
+        return new PostgresqlDbContext(optionsBuilder.Options);
+    }
+}
